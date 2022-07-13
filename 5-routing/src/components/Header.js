56c5acd16ca4a0f6cb10582e900cf361f0f877c3
yavuzsinan42 <yavuzsinan213@gmail.com>
2022-07-13 19:40:01 +0300

    useNavigate Hook Kullanımı

--- 5-routing/src/components/Header.js
+++ 5-routing/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 
 const Header = () => {
+    const navigate = useNavigate();
   return (
     <div>
         <ul className='menu'>
+            <li onClick={()=>navigate(-1)} className="geri-btn">Geri</li>
             <Link to='/'><li>Anasayfa</li></Link>
             <Link to='/users'><li>Kullanıcılar</li></Link>
             <Link to='/contact'><li>İletişim</li></Link>
