2acc4f1449abb4340fad50c444e714e86868a87b
yavuzsinan42 <yavuzsinan213@gmail.com>
2022-07-16 00:22:04 +0300

    useFormik Hook ile Form Yönetimi

--- 5-routing/src/pages/Dashboard/Contact.js
+++ 5-routing/src/pages/Dashboard/Contact.js
@@ -1,37 +1,55 @@
 import React from 'react'
-import {Formik, Field,Form} from 'formik'
+import { useFormik} from 'formik'
 const Contact = () => {
+  const formik = useFormik({ initialValues:{
+    firstName: '',
+    lastName: '',
+    email: '',
+  },
+  onSubmit:async (values) => {
+    await new Promise((r) => setTimeout(r, 500));
+    alert(JSON.stringify(values, null, 2));
+  },
+})
   return (
     <div>
       <h1>İletişim</h1>
-        <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
+      
+      
+        <form onSubmit={formik.handleSubmit} className="form">
+          <div>
         <label htmlFor="firstName">First Name</label>
-        <Field id="firstName" name="firstName" placeholder="Jane" />
-
+        <input id="firstName" name="firstName" placeholder="Jane" onChange={formik.handleChange("firstName")} />
+        </div>
+        <div>
         <label htmlFor="lastName">Last Name</label>
-        <Field id="lastName" name="lastName" placeholder="Doe" />
-
+        <input id="lastName" name="lastName" placeholder="Doe" onChange={formik.handleChange("lastName")} />
+        </div>
+        <div>
         <label htmlFor="email">Email</label>
-        <Field
+        <input
           id="email"
           name="email"
           placeholder="jane@acme.com"
+          onChange={formik.handleChange("email")}
           type="email"
         />
+        </div>
+        <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message"
+          onChange={formik.handleChange("Message")}
+          type="text"
+        />
+        </div>
         <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      </form>
+
+      
+  
     </div>
   )
 }
